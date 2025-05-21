e19e929b49674bfa57405025295794c7a377cd54
rius2k <f.unnys1ck@outlook.com>
2025-05-20 20:57:17 -0600

    posted

--- applications/nosotros.py
+++
@@ -1,7 +0,0 @@
-from flask import * #type: ignore
-from settings import *
-bp, app_config = Blueprint('nosotros', __name__), loadAppConfig() #type:ignore
-
-@bp.route('/')
-def index():
-    return 'Sobre nosotros'
--- applications/revista.py
+++
@@ -1,8 +0,0 @@
-from flask import * #type: ignore
-from settings import *
-bp, app_config = Blueprint('revista', __name__), loadAppConfig() #type:ignore
-
-
-@bp.route('/')
-def index():
-    return 'Nuestra revista'
